b8855977a9dd18414d303fbda61fdf888ce654be
Javier Puertas Rosello <javierpuertas2004@gmail.com>
2023-09-14 16:33:53 +0200

    Update de folder the .java

---
+++ Programacion/Pruebas/ejemplo2.java
@@ -0,0 +1,10 @@
+public class ejemplo2{
+    public static void main(String[] args){
+	double d = 100.5;
+        long l = (long)d;
+	int i = (int)l;
+	System.out.println("Valor doble  "+d);
+        System.out.println("Valor Long  "+l);
+        System.out.println("Valor int  "+i);
+    }
+}
---
+++ Programacion/Pruebas/test.java
@@ -0,0 +1,13 @@
+public class test {
+	public static void main(String[] args){
+
+		int i = 100;
+		long l = i;
+		float f = l;
+		
+		System.out.println("Valor Int  "+i);
+		System.out.println("Valor Long  "+l);
+		System.out.println("Valor Float  "+f);
+  		
+	}
+}
