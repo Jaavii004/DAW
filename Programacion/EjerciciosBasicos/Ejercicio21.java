af409cf112e7501f1816fe703b58a0d2ffe66c7a
Javier Puertas Rosello <javierpuertas2004@gmail.com>
2023-09-14 19:55:56 +0200

    Update

---
+++ Programacion/EjerciciosBasicos/Ejercicio21.java
@@ -0,0 +1,70 @@
+public class Ejercicio21 {
+    public static void main(String[] args) {
+        // Variables enteras
+        int p = 100;
+        int q = 200;
+        //Quiero que me explique el float
+        //Variables float
+        float x = 33.33f;
+        float y = 33.34F;
+        float z = 33.35f;
+        //Variables carácter
+        char a = 'a';
+        char b = 'b';
+        char c = 'c';
+        //Variables double
+        double raiz1 = 1.1;
+        double raiz2 = 2.2;
+        //Variable entera larga
+        long contador = 223456789L;
+        //Variable entera corta
+        short indicador = 123;
+        //Variable tipo byte
+        byte valor = 01;
+        //Variables lógicas
+        Boolean primero = false;
+        boolean ultimo = true;
+
+
+
+
+
+        
+
+        //Mostrar variables enteras 
+        System.out.println("Esto son variables enteras");
+        System.out.println("p = "+p);
+        System.out.println("q = "+q);
+        //Mostar Variables float
+        System.out.println("Esto son variables float");
+        System.out.println("x = "+x);
+        System.out.println("y = "+y);
+        System.out.println("z = "+z);
+        //Mostrar variables carácter
+        System.out.println("Esto son variables carácter");
+        System.out.println("a = "+a);
+        System.out.println("b = "+b);
+        System.out.println("c = "+c);
+        //Mostrar Variables double
+        System.out.println("Esto son variables double");
+        System.out.println("Raiz1 = "+raiz1);
+        System.out.println("Raiz2 = "+raiz2);
+        //Mostrar variable entera larga
+        System.out.println("Esto son variables entera larga");
+        System.out.println("contador = "+contador);
+        //Mostrar Variable entera corta
+        System.out.println("Esto son variables entera corta");
+        System.out.println("indicador = "+indicador);
+        //Mostrar Variable tipo byte
+        System.out.println("Esto son variables tipo byte");
+        System.out.println("valor = "+valor);
+        //Mostrar Variables double
+        System.out.println("Esto son variables double");
+        System.out.println("Raiz1 = "+raiz1);
+        System.out.println("Raiz2 = "+raiz2);
+
+
+
+
+    }
+}
