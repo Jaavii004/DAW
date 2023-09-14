0a5f626c8c76d07fc3c854ead67724fa8c329c89
Javier Puertas Rosello <javierpuertas2004@gmail.com>
2023-09-14 18:36:39 +0200

    update

--- Programacion/Pruebas/Hola.java
+++ Programacion/Pruebas/Hola.java
@@ -1,7 +1,5 @@
 //import 
-
-public class Hola
-{
+public class Hola {
 	public  static void main (String[]args)
 	{
 		System.out.println("Hola mundo");	
--- Programacion/Pruebas/ejemplo2.java
+++ Programacion/Pruebas/ejemplo2.java
@@ -1,9 +1,9 @@
 public class ejemplo2{
     public static void main(String[] args){
-	double d = 100.5;
+        double d = 100.5;
         long l = (long)d;
-	int i = (int)l;
-	System.out.println("Valor doble  "+d);
+        int i = (int)l;
+        System.out.println("Valor doble  "+d);
         System.out.println("Valor Long  "+l);
         System.out.println("Valor int  "+i);
     }
