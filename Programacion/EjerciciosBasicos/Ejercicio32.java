d6907533aabc2a26c22266693c358e021f8ec33e
Javier Puertas Rosello <javierpuertas2004@gmail.com>
2023-09-15 17:29:57 +0200

    Update Ejercicio32

---
+++ Programacion/EjerciciosBasicos/Ejercicio32.java
@@ -0,0 +1,18 @@
+public class Ejercicio32 {
+    public static void main(String[] args) {
+        double x = 88;
+        double y = 3.5;
+        double z = -5.2
+        x + y + z
+        2 * y + 3 * (x - z)
+        x / y
+        x % y
+        x / (y + z)
+        x * (y % z)
+        3 * x - z - 2 * x
+        2 * x / 5 % y
+        x - y - z * 2
+        
+    
+    }
+}
