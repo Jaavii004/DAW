f8beb15feba2d047230f8eb40dc5cf6705cb9da4
Javier Puertas Rosello <javierpuertas2004@gmail.com>
2023-09-15 18:08:20 +0200

    Update  Ejercicio33

---
+++ Programacion/EjerciciosBasicos/Ejercicio33.java
@@ -0,0 +1,33 @@
+public class Ejercicio33 {
+    public static void main(String[] args) {
+        char c1 = 'E';
+        char c2 = '5';
+        char c3 = '?';
+        int resulta = c1 + 1;
+        System.out.println("c1 + 1 = "+resulta);
+        int resultb = c1 - c2 + c3; 
+        System.out.println("c1 - c2 + c3 = "+resultb);
+    
+        int resultc = c1 - 2; 
+        System.out.println("c1 - 2 = "+resultc);
+    
+        int resultd = c2 - '2'; 
+        System.out.println("c2 - '2' = "+resultd);
+        
+        int resulte = c3 + '#'; 
+        System.out.println("c3 + '#' = "+resulte);
+        
+        int resultf = '2' + '2'; 
+        System.out.println("'2' + '2' = "+resultf);
+        
+        int resultg = 3 * c2; 
+        System.out.println("3 * c2 = "+resultg);
+        int resulth = '3' * c2; 
+        System.out.println("'3' * c2 = "+resulth);
+        
+
+
+
+
+    }
+}
