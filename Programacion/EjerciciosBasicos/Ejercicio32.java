aaa6fbfcf659b49079a09786e1ed29007c88a9a1
Javier Puertas Rosello <javierpuertas2004@gmail.com>
2023-09-15 17:52:26 +0200

    Update Ejercicio22

--- Programacion/EjerciciosBasicos/Ejercicio32.java
+++ Programacion/EjerciciosBasicos/Ejercicio32.java
@@ -2,17 +2,25 @@ public class Ejercicio32 {
     public static void main(String[] args) {
         double x = 88;
         double y = 3.5;
-        double z = -5.2
-        x + y + z
-        2 * y + 3 * (x - z)
-        x / y
-        x % y
-        x / (y + z)
-        x * (y % z)
-        3 * x - z - 2 * x
-        2 * x / 5 % y
-        x - y - z * 2
-        
-    
+        double z = -5.2;
+        double resulta = x + y + z;
+        double resultb = 2*y+3*(x-z);
+        double resultc = x/y;
+        double resultd = x % y; 
+        double resulte = x / (y + z); 
+        double resultf = x * (y % z); 
+        double resultg = 3 * x - z - 2 * x; 
+        double resulth = 2 * x / 5 % y; 
+        double resulti = x - y - z * 2; 
+
+        System.out.println("x + y + z = "+resulta);
+        System.out.println("2 * y + 3 * (x - z) = "+resultb);
+        System.out.println("x / y = "+resultc);
+        System.out.println("x % y = "+resultd);
+        System.out.println("x / (y + z) = "+resulte);
+        System.out.println("x * (y % z) = "+resultf);
+        System.out.println("3 * x - z - 2 * x = "+resultg);
+        System.out.println("2 * x / 5 % y = "+resulth);
+        System.out.println("x - y - z * 2 = "+resulti);
     }
 }
