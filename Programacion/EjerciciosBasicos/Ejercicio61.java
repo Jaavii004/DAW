dbe9d8cf91851d8dc6dff421214047615e59836d
jaavii_04 <javierpuertas2004@gmail.com>
2023-09-16 21:16:04 +0200

    Ejercio61 finish

--- Programacion/EjerciciosBasicos/Ejercicio61.java
+++ Programacion/EjerciciosBasicos/Ejercicio61.java
@@ -1,6 +1,6 @@
 class Ejercicio61 {
     public static void main(String[] args) {
-	byte b = 126;
+        byte b = 126;
         short s = 233;
         long ln = 2343434;
         int i  = 54;
@@ -8,20 +8,20 @@ class Ejercicio61 {
         float f = 54.4f;
         double d = 123.45;
         char c = 'c';
-	System.out.println("i + c =             "+(i + c));
+        System.out.println("i + c =             "+(i + c));
         System.out.println("b + c =             "+(b + c));
-	System.out.println("j - 4L =            "+(j - 4L));
-	System.out.println("f -c =              "+(f -c));
-	System.out.println("b / c + s =         "+(b / c + s));
-	System.out.println("j - 4L * 2.5F =     "+(j - 4L * 2.5F));
-	System.out.println("d + f =             "+(d + f));
-	System.out.println("c + c =             "+(c + c));
-	System.out.println("ln + c =            "+(ln + c));
-	System.out.println("'a' + c =           "+('a' + c));
-	System.out.println("s + j =             "+(s + j));
-	System.out.println("s * c =             "+(s * c));
-	System.out.println("i * f * 2.5 =       "+(i * f * 2.5));
-	System.out.println("2 / i + 2.0 / j  =  "+(2 / i + 2.0 / j ));
+        System.out.println("j - 4L =            "+(j - 4L));
+        System.out.println("f -c =              "+(f -c));
+        System.out.println("b / c + s =         "+(b / c + s));
+        System.out.println("j - 4L * 2.5F =     "+(j - 4L * 2.5F));
+        System.out.println("d + f =             "+(d + f));
+        System.out.println("c + c =             "+(c + c));
+        System.out.println("ln + c =            "+(ln + c));
+        System.out.println("'a' + c =           "+('a' + c));
+        System.out.println("s + j =             "+(s + j));
+        System.out.println("s * c =             "+(s * c));
+        System.out.println("i * f * 2.5 =       "+(i * f * 2.5));
+        System.out.println("2 / i + 2.0 / j  =  "+(2 / i + 2.0 / j ));
     }
 
 }
