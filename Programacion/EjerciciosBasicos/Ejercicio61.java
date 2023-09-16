947443b172157e7e83f279961c59116a61353cbe
jaavii_04 <javierpuertas2004@gmail.com>
2023-09-16 20:27:28 +0200

    Ejercio61 Start

---
+++ Programacion/EjerciciosBasicos/Ejercicio61.java
@@ -0,0 +1,13 @@
+public class Ejercicio51 {
+    public static void main(String[] args) {
+		System.out.println(" =               "+(!(c == 99)));
+		System.out.println(" =                 "+(!(x > 0)));
+		System.out.println(" =           "+(c > d || c > 0));
+		System.out.println(" =          "+(i <= j && i < 5));
+		System.out.println(" =           "+(i > 0 && j < 5));
+		System.out.println(" =           "+(i > 0 || j < 5));
+		System.out.println(" =  "+(x > y && i > 0 || j < 5));
+    }
+
+}
+
