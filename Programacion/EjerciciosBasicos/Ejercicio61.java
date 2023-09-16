45bfc1b43bbae9748fa75f58029619b832071d05
jaavii_04 <javierpuertas2004@gmail.com>
2023-09-16 21:09:50 +0200

    Ejercio61 finish

--- Programacion/EjerciciosBasicos/Ejercicio61.java
+++ Programacion/EjerciciosBasicos/Ejercicio61.java
@@ -1,35 +1,27 @@
-
-
-byte b;
-short s;
-long ln;
-int i, j;
-float f;
-double d;
-char c;
-Determina cuál es el tipo de dato del resultado de las siguientes expresiones aritméticas:
-a) i + c
-b) b + c
-c) j – 4L
-d) f – c
-e) b / c + s
-f) j – 4L * 2.5F
-g) d + f
-h) c + c
-i) ln + c
-j) ‘a’ + c
-k) s + j
-l) s * c
-m) i * f * 2.5
-n) 2 / i + 2.0 / j class Ejercicio51 {
+class Ejercicio61 {
     public static void main(String[] args) {
-		System.out.println(" =               "+(!(c == 99)));
-		System.out.println(" =                 "+(!(x > 0)));
-		System.out.println(" =           "+(c > d || c > 0));
-		System.out.println(" =          "+(i <= j && i < 5));
-		System.out.println(" =           "+(i > 0 && j < 5));
-		System.out.println(" =           "+(i > 0 || j < 5));
-		System.out.println(" =  "+(x > y && i > 0 || j < 5));
+	byte b = 126;
+        short s = 233;
+        long ln = 2343434;
+        int i  = 54;
+       	int j = 66 ;
+        float f = 54.4f;
+        double d = 123.45;
+        char c = 'c';
+	System.out.println("i + c =             "+(i + c));
+        System.out.println("b + c =             "+(b + c));
+	System.out.println("j - 4L =            "+(j - 4L));
+	System.out.println("f -c =              "+(f -c));
+	System.out.println("b / c + s =         "+(b / c + s));
+	System.out.println("j - 4L * 2.5F =     "+(j - 4L * 2.5F));
+	System.out.println("d + f =             "+(d + f));
+	System.out.println("c + c =             "+(c + c));
+	System.out.println("ln + c =            "+(ln + c));
+	System.out.println("'a' + c =           "+('a' + c));
+	System.out.println("s + j =             "+(s + j));
+	System.out.println("s * c =             "+(s * c));
+	System.out.println("i * f * 2.5 =       "+(i * f * 2.5));
+	System.out.println("2 / i + 2.0 / j  =  "+(2 / i + 2.0 / j ));
     }
 
 }
