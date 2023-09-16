f8c7c66d1b91a13eff7584fd06a8a7eee6143fb7
jaavii_04 <javierpuertas2004@gmail.com>
2023-09-16 19:49:34 +0200

    Ejercio51 finish

--- Programacion/EjerciciosBasicos/Ejercicio51.java
+++ Programacion/EjerciciosBasicos/Ejercicio51.java
@@ -1,7 +1,18 @@
 public class Ejercicio51 {
     public static void main(String[] args) {
-	!(c == 99)
-        
+	        int i = 8;
+		int j = 5;
+		float x = 0.005f;
+		float y = -0.01f;
+		char c = 'c';
+		char d = 'd';
+		System.out.println(!(c == 99));
+		System.out.println(!(x > 0));
+		System.out.println(c > d || c > 0);
+		System.out.println(i <= j && i < 5);
+		System.out.println(i > 0 && j < 5);
+		System.out.println(i > 0 || j < 5);
+		System.out.println(x > y && i > 0 || j < 5);
     }
 
 } 
