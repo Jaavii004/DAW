6669bdfdcd4a1f41fb616d37ad0e950e32488970
Jaavii_04 <116631009+Jaavii004@users.noreply.github.com>
2023-09-16 20:27:52 +0200

    Actualizar Ejercicio61.java

--- Programacion/EjerciciosBasicos/Ejercicio61.java
+++ Programacion/EjerciciosBasicos/Ejercicio61.java
@@ -1,4 +1,27 @@
-public class Ejercicio51 {
+
+
+byte b;
+short s;
+long ln;
+int i, j;
+float f;
+double d;
+char c;
+Determina cuál es el tipo de dato del resultado de las siguientes expresiones aritméticas:
+a) i + c
+b) b + c
+c) j – 4L
+d) f – c
+e) b / c + s
+f) j – 4L * 2.5F
+g) d + f
+h) c + c
+i) ln + c
+j) ‘a’ + c
+k) s + j
+l) s * c
+m) i * f * 2.5
+n) 2 / i + 2.0 / j class Ejercicio51 {
     public static void main(String[] args) {
 		System.out.println(" =               "+(!(c == 99)));
 		System.out.println(" =                 "+(!(x > 0)));
