62f0b90f28cc5787574bfb2cfa4aac3b96358a78
jaavii_04 <javierpuertas2004@gmail.com>
2023-09-17 12:27:01 +0200

    La tarea 2

---
+++ Programacion/EjerciciosBasicos2/Ejercicio02.java
@@ -0,0 +1,11 @@
+public class Ejercicio02 {
+    public static void main(String[] args) {
+        float a = 8.0f;
+        int b = 3;
+        char c = '5';
+        System.out.println("x + y =  "+(x + y));
+        System.out.println("x - y =  "+(x - y));
+        System.out.println("x / y =  "+(x / y));
+        System.out.println("x * y =  "+(x * y));
+    }
+}
--- Programacion/EjerciciosBasicos2/Ejercicio21.java
+++
@@ -1,5 +0,0 @@
-public class Ejercicio21 {
-    public static void main(String[] args) {
-        System.out.println();
-    }
-}
