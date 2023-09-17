490c4d99f8da46aab1ff3557e034f9fad7143762
jaavii_04 <javierpuertas2004@gmail.com>
2023-09-17 12:36:05 +0200

    La tarea 2

--- Programacion/EjerciciosBasicos2/Ejercicio02.java
+++ Programacion/EjerciciosBasicos2/Ejercicio02.java
@@ -1,26 +1,15 @@
-
-
-resint = (a - b + c);
-resdouble = (a - b + c);
-System.out.println("a + b + c = "+(a + b + c)+ " resint = "+resint+" resdouble = "+resdouble);
-a) a - b + c
-b) 2 * b + 3 * (a – c)
-c) a / b
-d) a % b
-e) a / c - 2
-f) a * b / c
-g) a * (c % b)
-h) (3 * a – 2 * b) % (2 * a – c)
-i) (a - 3 * b) % (c + 2 * a) / (a – c)
-j) a - b - c * 2
 class Ejercicio02 {
     public static void main(String[] args) {
         float a = 8.0f;
         int b = 3;
         char c = '5';
-        System.out.println("x + y =  "+(x + y));
-        System.out.println("x - y =  "+(x - y));
-        System.out.println("x / y =  "+(x / y));
-        System.out.println("x * y =  "+(x * y));
+        //int result;
+        //double resdubel;
+        int resint = (a-b+c);
+        double resdouble = (a-b+c);
+        System.out.println("a + b + c = "+(a + b + c)+ " resint = "+resint+" resdouble = "+resdouble);
+
+
+        System.out.println("x * y =  ");
     }
 }
