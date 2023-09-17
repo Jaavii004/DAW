b5f79fc7bb26bf29e33a6f1e668746e042e71588
jaavii_04 <javierpuertas2004@gmail.com>
2023-09-17 21:00:43 +0200

    Ejercicio04

---
+++ Programacion/EjerciciosBasicos2/Ejercicio04.java
@@ -0,0 +1,6 @@
+class Ejercicio04 {
+    public static void main(String[] args) {
+        String nombre = "Javier Puertas Rosello";
+        System.out.println("Buenos dias, mi nombre es "+nombre);
+    }
+}
