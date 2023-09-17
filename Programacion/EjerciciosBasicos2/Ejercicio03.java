2efbc716e03ab8e26f534641f9967a6566b380fe
jaavii_04 <javierpuertas2004@gmail.com>
2023-09-17 20:44:51 +0200

    Ejercicio03

---
+++ Programacion/EjerciciosBasicos2/Ejercicio03.java
@@ -0,0 +1,16 @@
+class Ejercicio03 {
+    public static void main(String[] args) {
+        float pi = 3.141592f;
+        double diam = 15.5;
+        double altura = 42.4;
+
+        System.out.println("pi = "+pi);
+        System.out.println("Diametro = "+diam);
+        System.out.println("altura = "+altura);
+
+        System.out.println("El area de este cilindro es de "+((2 * pi * (diam/2)) * altura));
+        System.out.println("El volumen de este cilindro es de "+(pi * (diam/2)));
+
+
+    }
+}
