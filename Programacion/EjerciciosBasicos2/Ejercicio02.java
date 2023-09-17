d79b0e5b7c8da9822ec6f66341028d67de270945
jaavii_04 <javierpuertas2004@gmail.com>
2023-09-17 20:21:17 +0200

    La tarea 2

--- Programacion/EjerciciosBasicos2/Ejercicio02.java
+++ Programacion/EjerciciosBasicos2/Ejercicio02.java
@@ -5,11 +5,46 @@ class Ejercicio02 {
         char c = '5';
         //int result;
         //double resdubel;
-        int resint = (a-b+c);
+        int resint = (int)(a-b+c);
         double resdouble = (a-b+c);
-        System.out.println("a + b + c = "+(a + b + c)+ " resint = "+resint+" resdouble = "+resdouble);
+        System.out.println("a - b + c = "+(a - b + c)+ " resint = "+resint+" resdouble = "+resdouble);
+
+        int resintb = (int)(2 * b + 3 * (a - c));
+        double resdoubleb = (2 * b + 3 * (a - c));
+        System.out.println("2 * b + 3 * (a - c) = "+(2 * b + 3 * (a - c))+ " resint = "+resintb+" resdouble = "+resdoubleb);
+
+        int resintc = (int)(a / b);
+        double resdoublec = (a / b);
+        System.out.println("a / b = "+(a / b)+ " resint = "+resintc+" resdouble = "+resdoublec);
 
 
-        System.out.println("x * y =  ");
+        int resintd = (int)(a % b);
+        double resdoubled = (a % b);
+        System.out.println("a % b = "+(a % b)+ " resint = "+resintd+" resdouble = "+resdoubled);
+
+        int resinte = (int)(a / c - 2);
+        double resdoublee = (a / c - 2);
+        System.out.println("a / c - 2 = "+(a / c - 2)+ " resint = "+resinte+" resdouble = "+resdoublee);
+
+        int resintf =  (int)(a * b / c);
+        double resdoublef = (a * b / c);
+        System.out.println("a * b / c = "+(a * b / c)+ " resint = "+resintf+" resdouble = "+resdoublef);
+
+        int resintg = (int)(a * (c % b));
+        double resdoubleg = (a * (c % b));
+        System.out.println("a * (c % b) = "+(a * (c % b))+ " resint = "+resintg+" resdouble = "+resdoubleg);
+
+        int resinth = (int)((3 * a - 2 * b) % (2 * a - c));
+        double resdoubleh = ((3 * a - 2 * b) % (2 * a - c));
+        System.out.println("(3 * a - 2 * b) % (2 * a - c) = "+((3 * a - 2 * b) % (2 * a - c))+ " resint = "+resinth+" resdouble = "+resdoubleh);
+
+        int resinti = (int)((a - 3 * b) % (c + 2 * a) / (a - c));
+        double resdoublei = ((a - 3 * b) % (c + 2 * a) / (a - c));
+        System.out.println("(a - 3 * b) % (c + 2 * a) / (a - c) = "+((a - 3 * b) % (c + 2 * a) / (a - c))+ " resint = "+resinti+" resdouble = "+resdoublei);
+
+        int resintj = (int)(a - b - c * 2);
+        double resdoublej = (a - b - c * 2);
+        System.out.println("a - b - c * 2 = "+(a - b - c * 2)+ " resint = "+resintj+" resdouble = "+resdoublej);
+
     }
 }
