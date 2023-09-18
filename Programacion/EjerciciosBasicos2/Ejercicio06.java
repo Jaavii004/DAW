073f3dd4ec081dfd06b39ec2f8635d8b1a249b0d
jaavii_04 <javierpuertas2004@gmail.com>
2023-09-18 22:54:29 +0200

    Update

---
+++ Programacion/EjerciciosBasicos2/Ejercicio06.java
@@ -0,0 +1,11 @@
+class Ejercicio04 {
+    public static void main(String[] args) {
+        String nombre = "Javier";
+        String primerape = "Puertas";
+        String segundoape = "Rosello";
+
+        System.out.println(nombre+" tiene una longitud de "+nombre.length());
+        System.out.println(primerape+" tiene una longitud de "+primerape.length());
+        System.out.println(segundoape+" tiene una longitud de "+segundoape.length());
+    }
+}
