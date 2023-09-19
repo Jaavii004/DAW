93673c5c4e57737c45dfb981b249dd4a3b150fe8
Javier Puertas Rosello <javierpuertas2004@gmail.com>
2023-09-19 20:43:13 +0200

    Update

---
+++ Programacion/EjerciciosAvanzados/Ejercicio01.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+class Ejercicio01 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+        int num;
+
+        System.out.print("Tabla: ");
+        num = sc.nextInt();
+
+        System.out.println("Vamos a mostrar la tabla del "+num);    
+    }
+
+}
