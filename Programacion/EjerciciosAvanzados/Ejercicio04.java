7850dcb94c09580ef350fc9521069dfccc484fcd
Javier Puertas Rosello <javierpuertas2004@gmail.com>
2023-09-21 16:02:06 +0200

    Update

---
+++ Programacion/EjerciciosAvanzados/Ejercicio04.java
@@ -0,0 +1,27 @@
+import java.util.*;
+class Ejercicio01 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+        double a;
+        double b;
+        double c;
+        double x;
+        double x2;
+        double resul;
+        System.out.print("a = ");
+        a = sc.nextDouble();
+        System.out.print("b = ");
+        b = sc.nextDouble();
+        System.out.print("c = ");
+        c = sc.nextDouble();
+        resul = Math.pow(b, 2);
+        resul = resul - 4 * a * c;
+        resul = Math.sqrt(resul);
+        x = (-b + resul)/ (2 * a);
+        x2 = (-b - resul)/ (2 * a);
+
+        System.out.println("x = "+x);
+        System.out.println("x2 = "+x2);
+
+    }
+}
