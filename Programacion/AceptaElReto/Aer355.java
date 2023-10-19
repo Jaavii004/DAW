a3d9d9db12986607a2987f244d0c57bbf532e7ac
Javier Puertas Rosello <javierpuertas2004@gmail.com>
2023-10-19 17:50:09 +0200

    Update

--- Programacion/AceptaElReto/Aer355.java
+++ Programacion/AceptaElReto/Aer355.java
@@ -1,21 +1,24 @@
 import java.util.Scanner;
 public class Aer355 {
     public static void main(String[] arg) {
-        int NumAlt;
-        int anio;
+        int NumAlt, anio;
         String muestreo = "";
         Scanner sc = new Scanner(System.in);
         NumAlt = sc.nextInt();
         for (int i = 1; i <= NumAlt; i++) {
-            do{ 
-                anio = sc.nextInt();
-            }while (anio <= 1581 || anio > 100000);
-            if ((anio % 4 == 0) && (anio % 100 != 0 || anio % 400 == 0)){
-                muestreo += "29";
-                muestreo += "\n";
+            anio = sc.nextInt();
+            if (anio % 4 == 0){
+                if (anio % 100 == 0){
+                    if (anio % 400 == 0){
+                        muestreo+="29\n";
+                    }else{
+                        muestreo+="28\n";
+                    }
+                }else{
+                    muestreo+="29\n";
+                }
             }else{
-                muestreo += "28";
-                muestreo += "\n";
+                muestreo+="28\n";
             }
         }
         System.out.print(muestreo);
