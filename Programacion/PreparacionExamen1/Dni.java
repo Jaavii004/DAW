bb84cad7531dc6b5d7a48aa5798398f155c7bbcb
Javier Puertas Rosello <116631009+Jaavii004@users.noreply.github.com>
2023-11-04 19:06:05 +0100

    Create Dni.java

---
+++ Programacion/PreparacionExamen1/Dni.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class DNI {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int Dni;
+        int resto;
+        char [] letras = {'T', 'R', 'W', 'A', 'G', 'M',  'Y', 'F', 'P',  'D', 'X', 'B',  'N', 'J', 'Z',  'S', 'Q', 'V',  'H', 'L', 'C',  'K', 'E'};
+        System.out.print("Yo te dire la letra ");
+        System.out.print("Numero del DNI: ");
+        Dni = sc.nextInt();
+        resto = Dni % 23;
+        System.out.print("Tu letra es "+letras[resto]);
+        System.out.print("Tu DNI queda asi "+Dni+letras[resto]);
+
+    }
+}
