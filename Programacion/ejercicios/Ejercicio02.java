77a36c05578a5baa2048bfcdf8d336ad76ed9a9b
Javier Puertas Rosello <javierpuertas2004@gmail.com>
2023-11-22 15:18:28 +0100

    Update

--- Programacion/ejercicios/Ejercicio02.java
+++ Programacion/ejercicios/Ejercicio02.java
@@ -4,11 +4,11 @@ public class Ejercicio02 {
     public static void main(String[] args) {
         int contador = 0;
         for (int i = 1000; i <= 9999; i++){
-            if (FNumDAW.esPrimo(i)) {
-                System.out.println(i+" es primo.");
+            if (FNumDAW.esCapicua(i)) {
+                System.out.println(i+" es capicua.");
                 contador++;
             }
         }
-        System.out.println("El numero total de numeros primos es: " + contador);
+        System.out.println("El numero total de numeros capicua es: " + contador);
     }
 }
