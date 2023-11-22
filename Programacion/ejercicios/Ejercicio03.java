f05490e38c1cc9392e8ecf2ed3da8a3d3675629d
Javier Puertas Rosello <javierpuertas2004@gmail.com>
2023-11-22 17:04:01 +0100

    Update

---
+++ Programacion/ejercicios/Ejercicio03.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+import funciones.FNumDAW;
+
+public class Ejercicio03 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num;
+        char letra;
+        int contador = 0;
+        num = sc.nextInt();
+        do {
+            String palabra = sc.next();
+            int suma = 0;
+            // Recorrer todas las letras de la palabra y sumarlas
+            for (int i = 0; i < palabra.length(); i++) {
+                letra = palabra.charAt(i);
+                suma += letra;
+            }
+            int antprim = FNumDAW.anteriorPrimo(suma);
+            System.out.println(antprim);
+            contador++;
+        } while (contador < num);
+        sc.close();
+    }
+}
