acf352b50b7f9a6425302618f7c4243f42326c37
Javier Puertas Rosello <javierpuertas2004@gmail.com>
2023-11-23 19:15:10 +0100

    Update

--- Sistemas/bat/saludos.bat
+++ Sistemas/bat/saludos.bat
@@ -4,5 +4,10 @@ echo.
 
 echo Nombre de usuario: %USERNAME%
 echo Nombre del ordenador: %COMPUTERNAME%
-echo Hora del sistema: %TIME%
+for /f "tokens=1-3 delims=:." %%a in ("%TIME%") do (
+    set "hora=%%a"
+    set "minutos=%%b"
+    set "segundos=%%c"
+)
+echo Hora del sistema: %hora%:%minutos%
 echo Fecha del sistema: %DATE%
