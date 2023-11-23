84d843820bc7fb1e21e9166cbeb300ee3bbe22c3
Javier Puertas Rosello <javierpuertas2004@gmail.com>
2023-11-23 16:30:10 +0100

    Update

--- Programacion/ejercicios/Ejercicio04er100.java
+++ Programacion/ejercicios/Ejercicio04er100.java
@@ -1,7 +1,5 @@
 import java.util.Scanner;
 import java.util.Arrays;
-import funciones.FNumDAW;
-
 public class Ejercicio04er100 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
@@ -10,21 +8,38 @@ public class Ejercicio04er100 {
         int num;
         int numdesc;
         int numasc;
+        int numi;
+        int[] digitos = new int[4];
         num = sc.nextInt();
         do {
-            numero = sc.nextInt();
-            numdesc = numero;
+            numi  = sc.nextInt();
+            numero = numi;
+            while (numi != 6174) {
 
-            // Ordenar en orden ascendente
-            Arrays.sort(numero);
-            
-            // Ordenar en orden descendente
-            //Arrays.sort(numero);
-            //Collections.reverse(Arrays.asList(numero));
-            
+                for (int j = 0; j < 4; j++) {
+                    digitos[j] = numero % 10;
+                    numero /= 10;
+                }
+                // Ordenar en orden ascendente
+                Arrays.sort(digitos);
+                numasc = digitos[0] * 1000 + digitos[1] * 100 + digitos[2] * 10 + digitos[3];
+                System.out.println("numasc "+ numasc);
+                
+                // Ordenar en orden descendente
+                for (int i = 0; i < 4; i++) {
+                    for (int j = i + 1; j < 4; j++) {
+                        if (digitos[i] < digitos[j]) {
+                            int temp = digitos[i];
+                            digitos[i] = digitos[j];
+                            digitos[j] = temp;
+                        }
+                    }
+                }
+                numdesc = digitos[0] * 1000 + digitos[1] * 100 + digitos[2] * 10 + digitos[3];
+                System.out.println("numdes "+numdesc);
+            }
             contador++;
         } while (contador < num);
         sc.close();
     }
-    
 }
