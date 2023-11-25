fb27fe18537e0f6c5236fe5521d8db94235474de
Javier Puertas Rosello <116631009+Jaavii004@users.noreply.github.com>
2023-11-25 11:58:31 +0100

    Actualizar settings.py

--- docker/repaso/CasoPractico2-Django/codigo/ejemplodjango/settings.py
+++ docker/repaso/CasoPractico2-Django/codigo/ejemplodjango/settings.py
@@ -79,6 +79,16 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+DATABASES = {
+'default': {
+'ENGINE': 'django.db.backends.postgresql',
+'NAME': 'postgres',
+'USER': 'postgres',
+'PASSWORD': 'postgres',
+'HOST': 'db',
+'PORT': 5432,
+}
+}
 
 
 # Password validation
