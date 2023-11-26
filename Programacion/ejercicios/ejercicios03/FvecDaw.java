fbd82cfa7253c070335496b0f384ff0596cca9da
jaavii_04 <javierpuertas2004@gmail.com>
2023-11-26 17:36:22 +0100

    Update

---
+++ Programacion/ejercicios/ejercicios03/FvecDaw.java
@@ -0,0 +1,37 @@
+package funciones;
+
+public class FvecDaw {
+    // Función GenerarArray:
+    public static int[] generarArrayInt(int n, int numMax, int numMin) {
+        int[] arrayale = new int[n];
+        int random;
+        for (int i = 0; i < n; i++) {
+            random = (int) (Math.random() * (numMax-numMin+1)+numMin);
+            arrayale[i] = random;
+        }
+        return arrayale;
+    }
+
+    // Función GenerarArray:
+    public static int minimoArrayInt(int[] array) {
+        int min;
+        min = array[0];
+        for (int i = 1; i < array.length; i++) {
+            if (min > array[i]) {
+                min =  array[i];
+            }
+        }
+        return min;
+    }
+
+    public static void main(String[] args) {
+        int[] contador = generarArrayInt(10, 9, 20);
+        for (int i = 0; i < 10; i++){
+            System.out.print(" " + contador[i]);
+        }
+
+        int compu = minimoArrayInt(contador);
+        System.out.println("/n " + compu);
+    }
+}
+
