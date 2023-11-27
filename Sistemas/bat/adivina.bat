45f8f7d18041260a8569b5a0f50b4eebf416ad27
Javier Puertas Rosello <116631009+Jaavii004@users.noreply.github.com>
2023-11-27 22:07:37 +0100

    Create adivina.bat

---
+++ Sistemas/bat/adivina.bat
@@ -0,0 +1,22 @@
+@echo off
+chcp 65001 > null
+
+title 
+
+set /p "min=mín: "
+set /p "max=max: "
+
+set /a random=%random% %% (max-min+1) + min
+
+:pregunta
+set /p "num=Numero: "
+
+:igual
+if %num% == %random% goto victoria else goto error
+
+:error
+echo El número aleatorio no es correcto. Sigue jugando
+goto pregunta
+
+:victoria
+echo El número aleatorio entre %min% y %max% es: %random% Enhorabuena
