720dd85cbc8189cb615cd37109285bf4b9def1e1
Javier Puertas Rosello <javierpuertas2004@gmail.com>
2023-11-28 20:31:03 +0100

    Update

--- Programacion/calculadoraRPN/CalculadoraRPN.java
+++ Programacion/calculadoraRPN/CalculadoraRPN.java
@@ -1,50 +1,110 @@
-import java.util.Stack;
+import java.util.Scanner;
 
 public class CalculadoraRPN {
-    public static void main(String[] args) {
-        // Código de prueba
-        String expresion = "5 2 + 3 *";
-        double resultado = calcularRPN(expresion);
-        System.out.println("El resultado es: " + resultado);
+    // Función Mostrarstak: Va a mostrar los numeros
+    public static void Mostrarstak(double[] array) {
+        for (int i = array.length-1; i >= 0; i--) {
+            String numerocon0 = String.format("%02d", (i+1));
+            System.out.println(numerocon0 + ":  " + array[i]);
+        }
+        System.out.println("Pulsa q para salir");
     }
 
-    public static double calcularRPN(String expresion) {
-        Stack<Double> pila = new Stack<>();
+    /*
+    // Calcula el número de elementos no nulos en un array de tipo double.
+    public static int contRelleno(double[] array) {
+        int relleno = 0;
+        for (int i = 0; i < array.length; i++) {
+            if (array[i]!=0) {
+                relleno +=1;
+            } 
+        }
+        return relleno;
+    }
+    */
 
-        String[] elementos = expresion.split(" ");
-        for (String elemento : elementos) {
-            if (elemento.matches("-?\\d+(\\.\\d+)?")) {
-                // Si es un número, lo agregamos a la pila
-                pila.push(Double.parseDouble(elemento));
+    public static double[] guardar(double[] array,double numGuardar) {
+        /*if (contRelleno(array)==array.length){
+            System.out.println("ESTOY LLENO NO ME CABE MAS");
+            return array;
+        }
+        */
+        double[] arrayvolteado = new double[array.length];
+        for (int i = 0; i < array.length-1; i++) {
+            arrayvolteado[i+1] = array[i];
+        }
+        arrayvolteado[0] = numGuardar;
+        return arrayvolteado;
+    }
+
+    public static double[] guardarResultados(double[] array,double numGuardar) {
+        double[] arrayvolteado = new double[array.length];
+        for (int i = 1; i < array.length-1; i++) {
+            arrayvolteado[i-1] = array[i+1];
+        }
+        arrayvolteado[0] = numGuardar;
+        return arrayvolteado;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num;
+        boolean salir;
+        String entrada = "";
+        double resultado = 0;
+        boolean valid;
+
+        System.out.print("Numero entre 4 y 20: ");
+        num = sc.nextInt();
+        double[] array = new double[num];
+
+        salir = false;
+        sc.nextLine();
+        while (!salir) {
+            valid = false;
+            Mostrarstak(array);
+            System.out.print("Entrada: ");
+            entrada = sc.nextLine();
+            if (entrada.matches("-?\\d+(\\.\\d+)?")) {
+                double numeroDouble = Double.parseDouble(entrada);
+                array = guardar(array, numeroDouble);
             } else {
-                // Si es un operador, realizamos la operación correspondiente
-                double num2 = pila.pop();
-                double num1 = pila.pop();
-                double resultado = 0;
+                double num1 = array[0];
+                double num2 = array[1];
 
-                switch (elemento) {
-                    case "+":
-                        resultado = num1 + num2;
-                        break;
-                    case "-":
-                        resultado = num1 - num2;
-                        break;
-                    case "*":
-                        resultado = num1 * num2;
-                        break;
-                    case "/":
-                        resultado = num1 / num2;
-                        break;
-                    default:
-                        throw new IllegalArgumentException("Operador inválido: " + elemento);
+                if (entrada.equalsIgnoreCase("q")) {
+                    salir = true;
+                    valid = true;
                 }
 
-                // Agregamos el resultado a la pila
-                pila.push(resultado);
+                if (entrada.equalsIgnoreCase("+")) {
+                    resultado = num1 + num2;
+                    array = guardarResultados(array, resultado);
+                    valid = true;
+                }
+
+                if (entrada.equalsIgnoreCase("*")) {
+                    resultado = num1 * num2;
+                    array = guardarResultados(array, resultado);
+                    valid = true;
+                }
+
+                if (entrada.equalsIgnoreCase("/")) {
+                    resultado = num2 / num1;
+                    array = guardarResultados(array, resultado);
+                    valid = true;
+                }
+
+                if (entrada.equalsIgnoreCase("-")) {
+                    resultado = num2 - num1;
+                    array = guardarResultados(array, resultado);
+                    valid = true;
+                }
+
+                if (!valid) {
+                    System.out.println("Opcion no valida");
+                }
             }
         }
-
-        // El resultado final estará en la cima de la pila
-        return pila.pop();
+        sc.close();
     }
 }
