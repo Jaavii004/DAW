50bff7f5b6515f8188a536b7ac1e630c0d248f24
Javier Puertas Rosello <javierpuertas2004@gmail.com>
2023-11-28 15:22:58 +0100

    Update

--- Sistemas/bat/limpia.bat
+++ Sistemas/bat/limpia.bat
@@ -4,6 +4,7 @@ chcp 65001 > nul
 title Limpia
 
 set extension=%1%
+REM Para ver si esta vacio o no
 if not "%extension%"=="" (
     echo Eliminamos los archivos ".%extension%"
     del C:\Users\javie\Downloads\*.%extension%
