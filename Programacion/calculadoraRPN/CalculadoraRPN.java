3e6f9afa7d92de9a0b4dc2d3eeaa4165436a8c11
Javier Puertas Rosello <javierpuertas2004@gmail.com>
2023-12-01 15:11:41 +0100

    update

--- Programacion/calculadoraRPN/CalculadoraRPN.java
+++ Programacion/calculadoraRPN/CalculadoraRPN.java
@@ -12,7 +12,7 @@ public class CalculadoraRPN {
     }
 
     /*
-    // Calcula el número de elementos vacios de un array .
+    // Calcula el número de elementos vacios de un array.
     public static int contRelleno(double[] array) {
         int relleno = 0;
         for (int i = 0; i < array.length; i++) {
