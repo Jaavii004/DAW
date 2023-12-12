04c4c55581834d65242cd29b92c1deadf2e480fa
Javier Puertas Rosello <javierpuertas2004@gmail.com>
2023-12-12 17:34:11 +0100

    Push bonoloto powershell

---
+++ Sistemas/powershell/bonoloto.ps1
@@ -0,0 +1,11 @@
+# Generar 6 números aleatorios diferentes entre 1 y 49
+$randomNumbers = Get-Random -Count 6 -Minimum 1 -Maximum 50 | Select-Object -Unique
+
+# Verificar si se generaron menos de 6 números únicos y generar los faltantes
+while ($randomNumbers.Count -lt 6) {
+    $missingNumbers = Get-Random -Count (6 - $randomNumbers.Count) -Minimum 1 -Maximum 50 | Select-Object -Unique
+    $randomNumbers += $missingNumbers
+}
+
+# Mostrar los números generados
+Write-Host "Los números de la Bonoloto son: $randomNumbers"
