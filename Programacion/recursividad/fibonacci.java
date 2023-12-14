3f4ab2332270eb532bf2bc2045310a818ab6c8e5
Javier Puertas Rosello <javierpuertas2004@gmail.com>
2023-12-14 15:05:27 +0100

    Update

--- Programacion/recursividad/fibonacci.java
+++ Programacion/recursividad/fibonacci.java
@@ -1,5 +1,17 @@
-public class Fibonacci {
-    public static void main(String[] args) {
-        // Your code here
+public class fibonacci{
+    public static int fibonaccii(int N) {
+        int res;
+        if (N == 0) {
+            res = 0;
+        } else if (N == 1) {
+            res=1;
+        } else {
+            res = fibonaccii(N - 1) + fibonaccii(N - 2);
+        }
+        return res;
+    }
+
+    public static void main (String[] args) {
+        System.out.println(fibonaccii(Integer.valueOf(args[0])));
     }
 }
