f756c8148904f314a28247a3ffbcb25bb0c25f3c
Javier Puertas Rosello <116631009+Jaavii004@users.noreply.github.com>
2023-12-19 16:14:17 +0100

    Update calculadora.ps1

--- Sistemas/powershell/calculadora.ps1
+++ Sistemas/powershell/calculadora.ps1
@@ -1,45 +1,7 @@
-function Sumar {
-    param (
-        [double]$num1,
-        [double]$num2
-    )
-    $resultado = $num1 + $num2
-    Write-Output "Suma: $resultado"
-}
+# Author Javier Puertas Rosello
 
-function Restar {
-    param (
-        [double]$num1,
-        [double]$num2
-    )
-    $resultado = $num1 - $num2
-    Write-Output "Resta: $resultado"
-}
-
-function Multiplicar {
-    param (
-        [double]$num1,
-        [double]$num2
-    )
-    $resultado = $num1 * $num2
-    Write-Output "Multiplicación: $resultado"
-}
-
-function Dividir {
-    param (
-        [double]$num1,
-        [double]$num2
-    )
-    if ($num2 -eq 0) {
-        Write-Output "Error: No se puede dividir por cero."
-    } else {
-        $resultado = $num1 / $num2
-        Write-Output "División: $resultado"
-    }
-}
-
-# Menú principal
 do {
+    # Mostrar menu
     Write-Host "Selecciona una operación:"
     Write-Host "1. Sumar"
     Write-Host "2. Restar"
@@ -50,11 +12,40 @@ do {
     $opcion = Read-Host "Ingresa el número de la operación"
 
     switch ($opcion) {
-        1 { Sumar (Read-Host "Ingresa el primer número") (Read-Host "Ingresa el segundo número") }
-        2 { Restar (Read-Host "Ingresa el primer número") (Read-Host "Ingresa el segundo número") }
-        3 { Multiplicar (Read-Host "Ingresa el primer número") (Read-Host "Ingresa el segundo número") }
-        4 { Dividir (Read-Host "Ingresa el primer número") (Read-Host "Ingresa el segundo número") }
-        5 { break }
-        default { Write-Host "Opción no válida. Inténtalo de nuevo." }
+        1 {
+            [double]$num1 = Read-Host "Ingresa el primer número"
+            [double]$num2 = Read-Host "Ingresa el segundo número"
+            PedrirNum
+            $resultado = $num1 + $num2
+            Write-Output "Suma: $resultado"
+        }
+        2 {
+            [double]$num1 = Read-Host "Ingresa el primer número"
+            [double]$num2 = Read-Host "Ingresa el segundo número"
+            $resultado = $num1 - $num2
+            Write-Output "Resta: $resultado" 
+        }
+        3 {
+            [double]$num1 = Read-Host "Ingresa el primer número"
+            [double]$num2 = Read-Host "Ingresa el segundo número"
+            $resultado = $num1 * $num2
+            Write-Output "Multiplicación: $resultado"
+        }
+        4 {
+            [double]$num1 = Read-Host "Ingresa el primer número"
+            [double]$num2 = Read-Host "Ingresa el segundo número"
+            if ($num2 -eq 0) {
+                Write-Output "Error: No se puede dividir por cero."
+            } else {
+                $resultado = $num1 / $num2
+                Write-Output "División: $resultado"
+            }
+        }
+        5 {
+            Write-host "Salimos"
+        }
+        default { 
+            Write-Host "Opción no válida. Inténtalo de nuevo." 
+        }
     }
 } while ($opcion -ne 5)
