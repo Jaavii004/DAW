c08f0f67f6d2229e1d499b2d5c0aa8ce24eea0f3
jaavii004 <116631009+Jaavii004@users.noreply.github.com>
2023-12-21 17:56:00 +0100

    Create piramidenumm.ps1

---
+++ Sistemas/powershell/prep/piramidenumm.ps1
@@ -0,0 +1,19 @@
+# Definir la altura de la pirámide
+$altura = 5
+
+# Bucle para cada nivel de la pirámide
+for ($i = 1; $i -le $altura; $i++) {
+    # Calcular la cantidad de espacios en blanco y asteriscos en cada nivel 
+    $espacios = $altura - $i
+    $asteriscos = $i * 2 - 1
+    # Imprimir espacios en blanco
+    for ($j = 1; $j -le $espacios; $j++) {
+        Write-Host -NoNewline (" ")
+    }
+
+    # Imprimir asteriscos
+    for ($o = 1; $o -le $asteriscos; $o++) {
+        Write-Host -NoNewline ($i)
+    }
+    Write-Host
+}
