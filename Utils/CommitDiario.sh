53e099031872bd3f0ed520a885b73f8568105871
Javier Puertas Rosello <javierpuertas2004@gmail.com>
2024-01-09 19:37:29 +0100

    Commit diario 2024-01-09 19:37:29

---
+++ Utils/CommitDiario.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Ruta al repositorio
+repositorio="/home/daw/DAW/"
+
+# Tu token de acceso al repositorio
+token="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"
+
+# Cambia al directorio del repositorio
+cd "$repositorio" || exit
+
+# Hacer commit
+git add .
+git commit -m "Commit diario $(date +'%Y-%m-%d %H:%M:%S')"
+
+# Push con el token
+git push origin main -u "$token"
+
+echo "Commit y push diario completados"
