5b4f0a0970db476e6125c1c39869f94eab5f03e0
Javier Puertas Rosello <javierpuertas2004@gmail.com>
2024-01-09 19:47:07 +0100

    Commit diario 09-01-2024 19:47

--- Utils/CommitDiario.sh
+++ Utils/CommitDiario.sh
@@ -7,7 +7,7 @@ repositorio="/home/daw/DAW/"
 # Tu nombre de usuario y contraseña
 usuario="jaavii004"
 contrasena="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"
-tu_repositorio_url="github.com/Jaavii004/DAW"
+tu_repositorio_url="DAW"
 # Cambia al directorio del repositorio
 cd "$repositorio" || exit
 
