746785f1eaa613ac94550630544be603ea8b7af1
Javier Puertas Rosello <javierpuertas2004@gmail.com>
2024-01-19 18:00:01 +0100

    Commit diario 19-01-2024 18:00

--- Programacion/AceptaElReto/AceptaElReto115.java
+++ Programacion/AceptaElReto/AceptaElReto115.java
@@ -1,11 +1,11 @@
+package Programacion.AceptaElReto;
+
 import java.util.Scanner;
 import java.util.Arrays;
-import funciones.FNumDAW;
 
 public class AceptaElReto115 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int contador = 0;
         int numero;
         int num;
         int numdesc;
@@ -15,50 +15,52 @@ public class AceptaElReto115 {
         int[] digitos = new int[4];
         boolean esKaprekar;
         num = sc.nextInt();
-        do {
+        for (int o = 1; o < num; o++) {
             numi  = sc.nextInt();
-            numero = numi;
-            esKaprekar = true;
-            numinteraciones = 0;
-            while (numi != 6174) {
-                numinteraciones++;
-                if (numinteraciones >= 8){
-                    numinteraciones = 8;
-                    numi = 6174;
-                    esKaprekar = false;
-                }
+            if (numi == 0) {
+                o = num;
+            }else{
                 numero = numi;
-                for (int j = 0; j < 4; j++) {
-                    digitos[j] = numero % 10;
-                    numero /= 10;
-                }
-                // Ordenar en orden ascendente
-                Arrays.sort(digitos);
-                numasc = digitos[0] * 1000 + digitos[1] * 100 + digitos[2] * 10 + digitos[3];
-                //System.out.println("numasc "+ numasc);
-                
-                // Ordenar en orden descendente
-                numdesc = FNumDAW.voltea(numero);
-                for (int i = 0; i < 4; i++) {
-                    for (int j = i + 1; j < 4; j++) {
-                        if (digitos[i] < digitos[j]) {
-                            int temp = digitos[i];
-                            digitos[i] = digitos[j];
-                            digitos[j] = temp;
+                esKaprekar = true;
+                numinteraciones = 0;
+                while (numi != 6174) {
+                    numinteraciones++;
+                    if (numinteraciones >= 8){
+                        numinteraciones = 8;
+                        numi = 6174;
+                        esKaprekar = false;
+                    }
+                    numero = numi;
+                    for (int j = 0; j < 4; j++) {
+                        digitos[j] = numero % 10;
+                        numero /= 10;
+                    }
+                    // Ordenar en orden ascendente
+                    Arrays.sort(digitos);
+                    numasc = digitos[0] * 1000 + digitos[1] * 100 + digitos[2] * 10 + digitos[3];
+                    //System.out.println("numasc "+ numasc);
+                    
+                    // Ordenar en orden descendente
+                    numdesc = digitos[3] * 1000 + digitos[2] * 100 + digitos[1] * 10 + digitos[0];
+                    for (int i = 0; i < 4; i++) {
+                        for (int j = i + 1; j < 4; j++) {
+                            if (digitos[i] < digitos[j]) {
+                                int temp = digitos[i];
+                                digitos[i] = digitos[j];
+                                digitos[j] = temp;
+                            }
                         }
                     }
+                    numdesc = digitos[0] * 1000 + digitos[1] * 100 + digitos[2] * 10 + digitos[3];
+                    numi = numdesc-numasc;
                 }
-                numdesc = digitos[0] * 1000 + digitos[1] * 100 + digitos[2] * 10 + digitos[3];
-                numi = numdesc-numasc;
-                //System.out.println(numdesc+" - "+numasc+" = "+numi);
-            }
-            if (esKaprekar) {
-                System.out.println("SI");
-            }else{
-                System.out.println("NO");
-            }
-            contador++;
-        } while (contador < num);
+                if (esKaprekar) {
+                    System.out.println("SI");
+                }else{
+                    System.out.println("NO");
+                }
+            }            
+        }
         sc.close();
     }
 }
