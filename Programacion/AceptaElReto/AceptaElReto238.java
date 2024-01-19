aa22a8941f2134229ff20de98bd94031fc5c3afa
Javier Puertas Rosello <javierpuertas2004@gmail.com>
2024-01-19 15:57:03 +0100

    Aceptados

--- Programacion/AceptaElReto/AceptaElReto238.java
+++ Programacion/AceptaElReto/AceptaElReto238.java
@@ -1,5 +1,6 @@
 package Programacion.AceptaElReto;
 
+import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.Scanner;
@@ -7,46 +8,47 @@ import java.util.Scanner;
 public class AceptaElReto238 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        Queue<Integer> billetes = new LinkedList<Integer>();
+        ArrayList<Queue<Integer>> arrayListDeColas = new ArrayList<>();
         boolean salir = false;
         int numBilletes = 0;
         int numPerso = 0;
-        int veces = 0;
-        int sumatotal = 0;
         String salida = "";
+        int billete = 0;
+
         while (!salir) {
             numBilletes = sc.nextInt();
             numPerso = sc.nextInt();
-            String[] salidaStrings = new String[numPerso];
+            for (int i = 0; i < numPerso; i++) {
+                Queue<Integer> cola = new LinkedList<Integer>();
+                arrayListDeColas.add(cola);
+            }
             if (numBilletes == 0 | numPerso == 0) {
                 salir = true;
             }else{
                 sc.nextLine();
+                int pers = 0;
                 for (int i = 1; i <= numBilletes; i++) {
-                    billetes.add(sc.nextInt());
-                    //System.out.println("persona "+i%numPerso);
-                }
-                for (int i = 0; i < numPerso; i++) {
-                    veces = 0;
-                    sumatotal = 0;
-                    salida = "";
-                    for (Integer tt : billetes) {
-                        veces++;
-                        //int pers = veces%numPerso;
-                        if (veces%numPerso == i) {
-                            salida += " ";
-                            salida += Integer.toString(tt);
-                            billetes.remove(veces);
-                            sumatotal += tt;
-                        }
+                    int bill = sc.nextInt();
+                    arrayListDeColas.get(pers).add(bill);
+                    pers++;
+                    if (pers == numPerso) {
+                        pers = 0;
                     }
-                    salidaStrings[i] = sumatotal + ":" + salida;
                 }
-                for (int i = numPerso-1; i >= 0; i--) {
-                    System.out.println(salidaStrings[i]);
+                int sumatotal = 0;
+                for (Queue<Integer> queue : arrayListDeColas) {
+                    sumatotal=0;
+                    salida = "";
+                    while (!queue.isEmpty()) {
+                        billete = queue.poll();
+                        salida += " "+billete;
+                        sumatotal += billete;
+                    }
+                    System.out.println(sumatotal+":"+salida);
                 }
+                sc.nextLine();
+                arrayListDeColas.clear();
                 System.out.println("---");
-                billetes.clear();
             }
         }
         sc.close();
--- Programacion/AceptaElReto/AceptaElReto238op.java
+++ Programacion/AceptaElReto/AceptaElReto238op.java
@@ -1,51 +1,54 @@
 package Programacion.AceptaElReto;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.Scanner;
 
 public class AceptaElReto238op {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        List<Integer> billetes = new ArrayList<>();
-        int numBilletes;
-        int numPerso;
-        
-        while (true) {
+        Queue<Integer> billetes = new LinkedList<Integer>();
+        boolean salir = false;
+        int numBilletes = 0;
+        int numPerso = 0;
+        int veces = 0;
+        int sumatotal = 0;
+        String salida = "";
+        while (!salir) {
             numBilletes = sc.nextInt();
             numPerso = sc.nextInt();
-            
-            if (numBilletes == 0 || numPerso == 0) {
-                break;
+            String[] salidaStrings = new String[numPerso];
+            if (numBilletes == 0 | numPerso == 0) {
+                salir = true;
+            }else{
+                sc.nextLine();
+                for (int i = 1; i <= numBilletes; i++) {
+                    billetes.add(sc.nextInt());
+                    //System.out.println("persona "+i%numPerso);
+                }
+                for (int i = 0; i < numPerso; i++) {
+                    veces = 0;
+                    sumatotal = 0;
+                    salida = "";
+                    for (Integer tt : billetes) {
+                        veces++;
+                        //int pers = veces%numPerso;
+                        if (veces%numPerso == i) {
+                            salida += " ";
+                            salida += Integer.toString(tt);
+                            billetes.remove(veces);
+                            sumatotal += tt;
+                        }
+                    }
+                    salidaStrings[i] = sumatotal + ":" + salida;
+                }
+                for (int i = numPerso-1; i >= 0; i--) {
+                    System.out.println(salidaStrings[i]);
+                }
+                System.out.println("---");
+                billetes.clear();
             }
-            
-            for (int i = 0; i < numBilletes; i++) {
-                billetes.add(sc.nextInt());
-            }
-            
-            int[] sumas = new int[numPerso];
-            StringBuilder[] salidas = new StringBuilder[numPerso];
-            
-            for (int i = 0; i < numPerso; i++) {
-                sumas[i] = 0;
-                salidas[i] = new StringBuilder();
-            }
-            
-            for (int i = 0; i < numBilletes; i++) {
-                int persona = i % numPerso;
-                int billete = billetes.get(i);
-                sumas[persona] += billete;
-                salidas[persona].append(billete).append(" ");
-            }
-            
-            for (int i = 0; i < numPerso; i++) {
-                System.out.println(sumas[i] + ": " + salidas[i].toString().trim());
-            }
-            
-            System.out.println("---");
-            billetes.clear();
         }
-        
         sc.close();
     }
 }
