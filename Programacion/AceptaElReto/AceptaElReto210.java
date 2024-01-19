4d30cf707bf222aee2ebe4453d879b200b29702d
Javier Puertas Rosello <javierpuertas2004@gmail.com>
2024-01-19 16:27:32 +0100

    Optimizando para el aceptado en Acepta el reto

--- Programacion/AceptaElReto/AceptaElReto210.java
+++ Programacion/AceptaElReto/AceptaElReto210.java
@@ -1,28 +1,26 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+package Programacion.AceptaElReto;
+
+import java.util.Scanner;
 
 public class AceptaElReto210 {
     public static boolean esPrimo(int numero) {
-        boolean resultado;
-        int numprim = 0;
-        int divi = 1;
-        if (numero % 1 != 0) {
-            divi = numero+1;
-            resultado = false;
+        if (numero <= 1) {
+            return false;
         }
-        while (divi <= numero) {
-            if (numero % divi == 0) {
-                numprim++;
+        if (numero <= 3) {
+            return true;
+        }
+        if (numero % 2 == 0 || numero % 3 == 0) {
+            return false;
+        }
+        int divisor = 5;
+        while (divisor * divisor <= numero) {
+            if (numero % divisor == 0 || numero % (divisor + 2) == 0) {
+                return false;
             }
-            divi++;
+            divisor += 6;
         }
-        if (numprim == 2) {
-            resultado = true;
-        } else {
-            resultado = false;
-        }
-        return resultado;
+        return true;
     }
 
     public static int anteriorPrimo(int numero) {
@@ -32,24 +30,34 @@ public class AceptaElReto210 {
         return numero;
     }
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         int num;
         char letra;
-        int contador = 0;
-        num = Integer.parseInt(br.readLine());
-        do {
-            String palabra = br.readLine();
-            int suma = 0;
-            // Recorrer todas las letras de la palabra y sumarlas
-            for (int i = 0; i < palabra.length(); i++) {
-                letra = palabra.charAt(i);
+        num = sc.nextInt();
+        sc.nextLine();
+        for (int i = 0; i < num; i++) {
+            String palabra = sc.nextLine();
+            int suma=0;
+            for (int j = 0; j < palabra.length(); j++) {
+                letra = palabra.charAt(j);
                 suma += letra;
             }
-            int antprim = anteriorPrimo(suma);
-            System.out.println(antprim);
-            contador++;
-        } while (contador < num);
-        br.close();
+            
+            System.out.println(anteriorPrimo(suma));
+        }
+        //do {
+        //    String palabra = sc.nextLine();
+        //    int suma = 0;
+        //    // Recorrer todas las letras de la palabra y sumarlas
+        //    for (int i = 0; i < palabra.length(); i++) {
+        //        letra = palabra.charAt(i);
+        //        suma += letra;
+        //    }
+        //    int antprim = anteriorPrimo(suma);
+        //    System.out.println(antprim);
+        //    contador++;
+        //} while (contador < num);
+        sc.close();
     }
 }
