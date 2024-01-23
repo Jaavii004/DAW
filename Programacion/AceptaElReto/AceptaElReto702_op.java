8c27a3db57d12498fc3e6157e595f0d668730dea
Javier Puertas Rosello <javierpuertas2004@gmail.com>
2024-01-23 20:35:00 +0100

    mejorado el codigo de el 702

---
+++ Programacion/AceptaElReto/AceptaElReto702_op.java
@@ -0,0 +1,41 @@
+package Programacion.AceptaElReto;
+
+import java.util.Scanner;
+import java.util.TreeMap;
+
+public class AceptaElReto702_op {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int total = sc.nextInt();
+        int mov = sc.nextInt();
+        TreeMap<String, Integer> meses = new TreeMap<>();
+
+        // añado los 12 meses a 0 recorriendolos todos
+        for (int i = 1; i <= 12; i++) {
+            String MesCon0 = String.format("%02d", i);
+            meses.put(MesCon0, 0);
+        }
+
+        // voy a obtener la entrada y quedarme solo con el mes
+        // despues lo añado sumando a si habia algo antes si no al 0 inicializado al principio
+        for (int i = 0; i < mov; i++) {
+            String mes = sc.next();
+            int Dineromes = sc.nextInt();
+            sc.nextLine();
+            mes = mes.substring(mes.indexOf("-")+1);
+            meses.put(mes, meses.get(mes) + Dineromes); 
+        }
+
+        // 
+        for (String mesesString : meses.keySet()) {
+            int dinDelMes = meses.get(mesesString);
+            total += dinDelMes;
+            if (mesesString.equals("12")) {
+                System.out.print(total);
+            } else {
+                System.out.print(total + " ");
+            }
+        }
+        sc.close();
+    }
+}
