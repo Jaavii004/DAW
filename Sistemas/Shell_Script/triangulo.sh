55e92c9652b3f023d5d324c0eaf1459cc357c134
Javier Puertas Rosello <javierpuertas2004@gmail.com>
2024-03-13 17:29:25 +0100

    Update

--- Sistemas/Shell_Script/triangulo.sh
+++ Sistemas/Shell_Script/triangulo.sh
@@ -2,18 +2,31 @@
 
 for i in $(seq 1 5); do
     for e in $(seq $i 5); do
-        echo -n $e" "
+        echo -n "*"
     done
     echo
 done
 
-
+echo
 for u in $(seq 1 5); do
     for ((i = 1; i <= $u; i++)); do
-        echo -n $i" "
+        echo -n "*"
     done
     echo
 done
 
+echo 
+for j in $(seq 1 5); do
+    for ((i = 5; i >= $j; i--)); do
+        echo -n " "
+    done
+    for ((i = 1; i <= $j; i++)); do
+        echo -n "*"
+    done
+    for ((i = 2; i <= $j; i++)); do
+        echo -n "*"
+    done
+    echo
+done
 
 exit 0
