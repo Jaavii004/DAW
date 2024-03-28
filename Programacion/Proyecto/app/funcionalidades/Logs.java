9baa1ebd3787a2cbcf324717a40ff56a6f988952
jaavii_04 <116631009+Jaavii004@users.noreply.github.com>
2024-03-28 13:02:16 +0100

    Update Logs.java

--- Programacion/Proyecto/app/funcionalidades/Logs.java
+++ Programacion/Proyecto/app/funcionalidades/Logs.java
@@ -1,3 +1,4 @@
+import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
@@ -7,12 +8,20 @@ import java.util.Date;
 public class Logs {
     private static String rutalog = "log/";
     private static String Fecha = new SimpleDateFormat("yyyy-MM-dd").format(new Date());
+    private static String hora = new SimpleDateFormat("hh:mm:ss").format(new Date());
 
     public static void log(String mensaje) {
-        String nombreArchivo = rutalog + "miniAdv - " + Fecha + ".log";
-        try (
-            PrintWriter fw = new PrintWriter(new FileWriter(nombreArchivo, true))) {
-            fw.println(Fecha + " - \n " + mensaje);
+        File directorio = new File(rutalog);
+        if (!directorio.exists()) {
+            if (directorio.mkdirs()) {
+                System.out.println("Directorio creado correctamente.");
+            }
+        }
+        String nombreArchivo = rutalog + Fecha + ".log";
+        try {
+            PrintWriter fw = new PrintWriter(new FileWriter(nombreArchivo, true));
+            fw.println(hora + "\n" + mensaje);
+            fw.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
