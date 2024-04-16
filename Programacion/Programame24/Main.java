da0f081abd599cd047808072b68b21e9ae1a4774
Javier Puertas Rosello <javierpuertas2004@gmail.com>
2024-04-16 18:00:01 +0200

    Update

---
+++ Programacion/Programame24/Main.java
@@ -0,0 +1,14 @@
+package Programacion.Programame24;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("");
+        Scanner sc = new Scanner(System.in);
+
+        sc.close();
+    }
+    
+}
