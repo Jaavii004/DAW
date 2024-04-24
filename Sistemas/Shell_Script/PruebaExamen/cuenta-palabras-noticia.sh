a5460c20be505607c3070d3ff37d67e9fc93a6a3
jaavii_04 <116631009+Jaavii004@users.noreply.github.com>
2024-04-24 14:52:51 +0000

    Update

---
+++ Sistemas/Shell_Script/PruebaExamen/cuenta-palabras-noticia.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+if [ "$#" -ne 1 ]; then
+    echo "uso: *$0* Nombre_archivo_noticia"
+    exit 1
+fi
+
+archivo=$1
+
+meses=("ENERO" "FEBRERO" "MARZO" "ABRIL" "MAYO" "JUNIO" "JULIO" "AGOSTO" "SEPTIEMBRE" "OCTUBRE" "NOVIEMBRE" "DICIEMBRE")
+
+while IFS="$" read -r ID FECHA TITULO AUTOR TEXTO; do
+    mes=$(date -d "$FECHA" +"%m")
+    nombrearchi="palabras-"${meses[$mes-1]}".txt"
+    contarpala=$(echo $TEXTO | wc -w)
+    MAY=$(echo "$TITULO" | tr '[:lower:]' '[:upper:]')
+    frase="$AUTOR, $MAY, $contarpala"
+    if ! grep -q "$frase" "$nombrearchi" ; then
+        echo "$frase" >> "$nombrearchi"
+    fi
+done < /NOTICIAS/$archivo
+
+exit 0
